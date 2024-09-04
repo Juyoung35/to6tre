915ae893e4c7724269146351eda315a43a14557d
Juyoung35 <68434709+Juyoung35@users.noreply.github.com>
2024-09-04 11:13:15 +0000

    ron

--- src/gpg/games.rs
+++ src/gpg/games.rs
@@ -1,3 +1,28 @@
 mod minesweeper {
     
 }
+
+enum TentsAndTreesCell {
+    #[default]
+    Empty {
+        l_click: Tent,
+        r_click: Flagged,
+        color: WHITE,
+    },
+    Flagged {
+        l_click: Tent,
+        r_click: Empty,
+        color: WHITE,
+    },
+    #[random(0)]
+    Tree {
+        color: GREEN,
+        sprite: "assets/tree.svg",
+    },
+    Tent {
+        l_click: Empty,
+        r_click: Flagged,
+        color: GREEN,
+        sprite: "assets/tent.svg",
+    },
+}
